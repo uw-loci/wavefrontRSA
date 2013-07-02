f2fa9f7e3d748e5985f2497aaeaef0cc5da43a71
Bosh Liu <bliuforgit@gmail.com>
2013-07-02 09:44:37 -0500

    Added blank control and force working sequence
    A picture of a blank control is now taken after each phase mask. CCD now
    captures image 0.1s after BNS_SendImageFrameToSLM command to ensure
    phase mask is displayed before an image is captured.

--- Main.m
+++ Main.m
@@ -1,4 +1,6 @@
 %% initilize
+tic;
+pause on;
 profile off;
 % profile clear;
 disp('Initializing variables...');
@@ -8,12 +10,13 @@ CCDHeight = 600;
 % ImageIndex = 1;
 % EArray = zeros(64,64,16);
 % phase = zeros(512,512);
-rectSideLen = 8; % the length of unit rectangle on SLM display. All pixels in this rectangle has the same phase
+rectSideLen = 16; % the length of unit rectangle on SLM display. All pixels in this rectangle has the same phase
 phaseStep = 8; % the step size of phase modulation.
 E = zeros(256/phaseStep,1);
-filePath = 'C:\Documents and Settings\zeeshan\Desktop\SLMCCDData062813_3\';
+filePath = 'C:\Documents and Settings\zeeshan\Desktop\SLMCCDData070113\';
 wmatrix = gauss2D(CCDHeight,CCDWidth,15);
 % ImageData = uint8(zeros(512,512));
+blank = uint8(zeros(512,512));
 ImageData = Mosaic(rectSideLen,phaseStep);
 %load('C:\Documents and Settings\zeeshan\My Documents\MATLAB\SLMCCD.v3\weightingMatrix.mat');
 previousEmax = 0;
@@ -24,7 +27,9 @@ disp('initializing SLM...');
 BNS_OpenSLM();
 BNS_SetPower(true);
 handles.slm_lut = BNS_ReadLUTFile('C:\BNSMatlabSDK\LUT_Files\linear.LUT');
-
+BNS_LoadImageFrame(0, blank, handles);
+BNS_LoadImageFrame(1, blank, handles);
+BNS_LoadImageFrame(2, blank, handles);
 %% initilize CCD
 disp('initializing CCD...');
 vidobj = videoinput('dcam', 1, 'Y8_800x600');
@@ -32,7 +37,7 @@ vidobj = videoinput('dcam', 1, 'Y8_800x600');
 % set the properties of video object
 src = getselectedsource(vidobj);
 src.GainMode = 'manual';
-src.FrameTimeout = 50000;
+src.FrameTimeout = 500000;
 src.Gain = 400;
 src.AutoExposure = 106;
 src.Brightness = 339;
@@ -53,7 +58,7 @@ start(vidobj);
 disp('initializing text file...');
 logFilePath = strcat(filePath,'FocusEfficiency.txt');
 fileID = fopen(logFilePath,'w');
-fprintf(fileID,'%3s %3s %5s %10s\r\n','row','col','phase','eff');
+fprintf(fileID,'%3s %3s %5s %10s %10s\r\n','row','col','phase','eff', 'ctr');
 
 %% Loop
 for n = 1:rectSideLen:512
@@ -62,11 +67,12 @@ for n = 1:rectSideLen:512
         for p = 0:phaseStep:255
             ImageData(n:n+rectSideLen-1,m:m+rectSideLen-1) = p;
             
-            FrameNum = mod(FrameNum + 1, 2);
-            
+            %% display the phase mask
+                       
             % sending image to SLM
             BNS_LoadImageFrame(FrameNum, ImageData, handles);
             BNS_SendImageFrameToSLM(FrameNum);
+            pause(0.1);
             
             % getting response from CCD
             % snapshot = getsnapshot(vidobj);
@@ -78,7 +84,26 @@ for n = 1:rectSideLen:512
             E((p/phaseStep)+1) = currentE;
             
             % display status reports 
-            StatusReport(n, m, p, currentE , fileID, snapshot, filePath);
+            % StatusReport(n, m, p, currentE , fileID, snapshot, filePath);
+            
+            FrameNum = mod(FrameNum + 1, 2);
+            
+            %% display the blank control 
+                        
+            % sending image to SLM
+            BNS_SendImageFrameToSLM(2);
+            pause(0.1);
+            % getting response from CCD
+            % snapshot = getsnapshot(vidobj);
+            trigger(vidobj);
+            blank = getdata(vidobj);
+            
+            % calculate the focus efficiency
+            controlE = FocusEff(blank,wmatrix);
+            % E((p/phaseStep)+1) = currentE;
+            
+            % display status reports 
+            StatusReport(n, m, p, currentE , controlE, fileID, snapshot, blank, filePath);
             
         end
         E_index = (E == max(E));
@@ -97,6 +122,9 @@ for n = 1:rectSideLen:512
         % EArray(n,m,:) = E;
     end
 end
-% save('C:\Documents and Settings\zeeshan\Desktop\SLMCCDData062713\phase.mat','phase')
+phaseMaskPath = strcat(filePath,'phaseMask.mat');
+save(phaseMaskPath,'ImageData');
+% stoppreview(vidobj);
 stop(vidobj);
 BNS_ClosesSLM();
+time = toc;
