b304c4f920a1b76b791af5a560f88742e9e85499
Bosh Liu <jollier.liu@gmail.com>
2013-08-20 17:54:54 -0500

    Moved getImage into  imageAcquisition/ and loadFocusEfficiency.m into dataAnalysis/ with the new name plotData.m

--- loadFocusEfficiency.m
+++
@@ -1,6 +0,0 @@
-file = dlmread('C:\Documents and Settings\zeeshan\Desktop\reflectance0812\RTratio.txt');
-plot(file(:,5),'r')
-hold on 
-plot(file(:,6),'g')
-plot(file(:,7),'k')
-legend('Current Reflectance','Best Reflectance','Control')
