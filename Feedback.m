631bd090717ca278ded097aa355f3e970d421d5f
Bosh Liu <jollier.liu@gmail.com>
2013-08-20 21:40:45 -0500

    deleted Feedback.m b/c it is no longer needed.

--- Feedback.m
+++
@@ -1,8 +0,0 @@
-function [ move ] = Feedback( E , ImageIndex )
-%FEEDBACK accepts E and Image Index. If E(ImageIndex) is the global minimum
-%the array E, then move is true. 
-
-move = (E(ImageIndex) == min(E(1:ImageIndex)));
-
-end
-
