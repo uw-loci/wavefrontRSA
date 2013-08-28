9735dd060481a5cf98dbc3f3f76d19eb1af7858f
Bosh Liu <jollier.liu@gmail.com>
2013-08-28 11:51:20 -0500

    deleted extraneous files

--- FocusEff.m
+++
@@ -1,9 +0,0 @@
-function [ E ] = FocusEff( snapshot )
-%FocusEff calculates Intensity(center)/Intensity(periphery) of the input
-%image and output the efficiency as E. 
-
-% E = sum(sum(double(snapshot).*weightingMatrix))/sum(sum(snapshot));
-E = sum(sum(snapshot));
-
-end
-
